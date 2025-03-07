b3929a372e65d2d3d8de6eb5a9723e57d3bfda35
vishal <vishalp.brainerhub@gmail.com>
2025-03-07 15:17:56 +0530

    Update the services

--- data_import/models/order_export.py
+++ data_import/models/order_export.py
@@ -14,7 +14,7 @@ class OrderExportCron(models.Model):
 
     def _export_orders(self):
         try:
-            out_dir = '/home/dell/Documents/Projects/PrimaPaint/odoo-15.0/primapaint_addons/Orders/controllers'
+            out_dir = os.environ.get('EXPORT_OUTPUT_DIR')
             os.makedirs(out_dir, exist_ok=True)
             filename = f'{out_dir}/orders_export.csv'
             
